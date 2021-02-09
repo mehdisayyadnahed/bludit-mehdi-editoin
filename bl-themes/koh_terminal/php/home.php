26e5b0121c1214294c9877132d7fbe02c32e1974
MaCo <manvacode@chmail.ir>
2021-02-09 04:14:20 +0330

    اضافه کردن یک فاصله بین عنوان و مطلب

--- bl-themes/koh_terminal/php/home.php
+++ bl-themes/koh_terminal/php/home.php
@@ -43,6 +43,7 @@
 					<div class="card-title">
 						<h3 class="text-primary"><?php echo $page->title(); ?></h3>
 					</div>
+					<br>
 					<!-- <p class="text-info card-subtitle mb-3"><?php #echo $page->date(); ?> - <?php #echo $L->get('Reading time') . ': ' . $page->readingTime(); ?></p> -->
 					<div class="card-text">
 						<?php //echo (substr(($page->contentBreak()),0, 200)." ... "); ?>
